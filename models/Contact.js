3f587c0673cbfcbd9830531fc554af6de18d4ed6
monikabarde15 <monikabarde15@gmail.com>
2025-08-26 14:50:23 +0530

    contact

--- models/Contact.js
+++ models/Contact.js
@@ -1,13 +1,14 @@
 // models/Contact.js
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const contactSchema = new mongoose.Schema({
-  firstName: String,
-  email: String,
-  phone: String,
-  service: String,
-  message: String,
-  date: { type: Date, default: Date.now }
-});
+const contactSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Contact', contactSchema);
+export default mongoose.model("Contact", contactSchema);
