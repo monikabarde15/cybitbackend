3863fb3eee1c79a6861a67e2fa2fa2cb27f9120c
monikabarde15 <monikabarde15@gmail.com>
2025-08-28 18:28:57 +0530

    upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,26 +1,24 @@
-import AWS from "aws-sdk";
 import multer from "multer";
 import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION || "ap-south-1",
+const s3 = new S3Client({
+  region: "ap-south-1", // replace with your region
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
 });
-
-console.log("S3 Bucket:", process.env.S3_BUCKET_NAME);
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.S3_BUCKET_NAME,
+    bucket: process.env.S3_BUCKET_NAME, // ✅ must match env
     acl: "public-read",
-    key: (req, file, cb) => {
-      const folder = file.fieldname === "resume" ? "resumes/" : "blogs/";
-      cb(null, `${folder}${Date.now()}_${file.originalname}`);
+    key: function (req, file, cb) {
+      cb(null, `${Date.now()}_${file.originalname}`);
     },
   }),
 });
